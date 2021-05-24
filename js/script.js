9fba5f18a517bc6624eeea29f32894eda874142b
Jaroslav <new.pustavit@gmail.com>
2021-05-24 22:05:26 +0300

    Finish first app

--- js/script.js
+++ js/script.js
@@ -1,2 +1,19 @@
-// hello
-// second hello
+const numberOfFilms = +prompt("Сколько фильмов вы посмотрели?", "");
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt("Один из последних:", ""),
+      b = prompt("На сколько оцените", ""),
+      с = prompt("Один из последних:", ""),
+      d = prompt("На сколько оцените", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[с] = d;
+
+console.log(personalMovieDB);
